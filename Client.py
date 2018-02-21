7a3a7210eb7e048dd96815cbeafa932027b0ed09
arnaskap <arnas.kapustinskas@gmail.com>
2018-02-21 18:49:39 +0000

    Finished initial implementation of traffic gen

--- Client.py
+++ Client.py
@@ -20,17 +20,17 @@ class Client(CircuitUser):
         self.general_circuit = None
         # Can have one active Client - Intro Point circuit for every
         # different hidden service establishing an RP with
-        self.c_ip_circuit = {}
+        self.c_ip_circuits = {}
         # Can have one active Client - Rendezvous Point circuit for
         # every different hidden service being visited
-        self.c_rp_circuit = {}
+        self.c_rp_circuits = {}
 
         # Hidden service address to introduction point map
         self.ips = ips
         # Rendezvous points for hidden services
         self.rps = {}
         # User IDs for hidden services
-        self.hs_user_id = {}
+        self.hs_user_ids = {}
 
     # Get a random relay as the rendezvous point to be used
     def _get_new_rp(self):
@@ -46,40 +46,48 @@ class Client(CircuitUser):
     def _establish_c_ip_circuit(self, hs_address):
         circuit = self._get_new_circuit(type='C-RP', time=self.time)
         self.time += circuit.lived
-        self.c_ip_circuit[hs_address] = circuit
+        self.c_ip_circuits[hs_address] = circuit
 
     # Set new circuit as Client to RP circuit for given hidden
     # service
     def _establish_c_rp_circuit(self, hs_address):
         circuit = self._get_new_circuit(type='C-RP', time=self.time)
         self.time += circuit.lived
-        self.c_rp_circuit[hs_address] = circuit
+        self.c_rp_circuits[hs_address] = circuit
 
     # Send request to HS intro point asking to start communication
     # through specified rendezvous point
     def _send_to_ip(self, hs_address, rp_id):
         ip = self.ips[hs_address]
-        user_id = self.hs_user_id[hs_address]
+        user_id = self.hs_user_ids[hs_address]
         out_content = 'IP {0} {1} {2}'.format(hs_address, rp_id, user_id)
         packet = Packet(self.id, self.time, content=out_content)
-        self.c_ip_circuit[hs_address].send_packets([packet], ip)
+        self.c_ip_circuits[hs_address].send_packets([packet], ip)
 
     # Send first packet to RP through established C-RP circuit,
     # nominating it for HS communication
     def _nominate_rp(self, hs_address):
-        user_id = self.hs_user_id[hs_address]
+        user_id = self.hs_user_ids[hs_address]
         out_content = 'RP-C {0} {1}'.format(hs_address, user_id)
         rp_packet = Packet(self.id, self.time, content=out_content)
         rp = self._get_new_rp()
-        self.c_rp_circuit[hs_address].send_packets([rp_packet], rp)
+        self.c_rp_circuits[hs_address].send_packets([rp_packet], rp)
         self.time += rp_packet.lived
-        return rp.id
+        return rp
 
     def _establish_hs_connection(self, hs_address):
         self._establish_c_rp_circuit(hs_address)
-        rp_id = self._nominate_rp(hs_address)
+        rp = self._nominate_rp(hs_address)
+        self.rps[hs_address] = rp
         self._establish_c_ip_circuit(hs_address)
-        self._send_to_ip(hs_address, rp_id)
+        self._send_to_ip(hs_address, rp.id)
+
+    def _process_packet(self, sender, packet, circuit=None):
+        in_content = packet.split(' ')
+        packet_type = in_content[0]
+        if packet_type.startswith('RP-confirm') or \
+           packet_type.startswith('RP-data'):
+            self.time = packet.creation_time + packet.lived
 
     # Emulates packet sending for visiting a specified clearnet website
     def visit_clearnet_site(self, site):
@@ -89,15 +97,23 @@ class Client(CircuitUser):
         # GET request packet for specified site
         get_packet = Packet(self.id, self.time,
                             content='GET {0}'.format(site.id))
-        self.general_circuit.send_packets(get_packet)
+        self.general_circuit.send_packets([get_packet], site)
         self.time += get_packet.lived
 
     # Emulates process of establishing connection to hidden service
     # and making requests / receiving responses from it
     def visit_hidden_service(self, hs_address):
-        if not hs_address in self.hs_user_id:
+        if not hs_address in self.hs_user_ids:
             hs_uid = uuid.uuid4()
-            self.hs_user_id[hs_address] = hs_uid
+            self.hs_user_ids[hs_address] = hs_uid
 
-        if not hs_address in self.c_rp_circuit or \
-           not self.c_rp_circuit[hs_address].is_running:
+        if not hs_address in self.c_rp_circuits or \
+           not self.c_rp_circuits[hs_address].is_running:
+            self._establish_hs_connection(hs_address)
+
+        user_id = self.hs_user_ids[hs_address]
+        c_rp_circuit = self.c_rp_circuits[hs_address]
+        rp = self.rps[hs_address]
+        out_content = 'RP-GET {0} {1}'.format(hs_address, user_id)
+        packet = Packet(self.id, self.time, content=out_content)
+        c_rp_circuit.send_packets([packet], rp)
--- HiddenService.py
+++ HiddenService.py
@@ -2,13 +2,14 @@
 # the Tor network
 
 import hashlib
+import math
 
 from CircuitUser import *
 
 
 class HiddenService(CircuitUser):
 
-    def __init__(self, id, time, bandwidth, continent, relays, pos_guards,
+    def __init__(self, id, time, bandwidth, continent, size, relays, pos_guards,
                  pos_middles, pos_exits, intro_points, tracked=False):
         super().__init__(id, time, bandwidth, continent, relays,
                          pos_guards, pos_middles, pos_exits, tracked)
@@ -22,6 +23,8 @@ class HiddenService(CircuitUser):
         self.hs_rp_circuits = {}
         # Maps users to their used rendezvous points
         self.user_rp = {}
+        # Size of website
+        self.size = size
 
         # Hash address counter for ensuring unique hashes
         self.hash_counter = 0
@@ -32,6 +35,16 @@ class HiddenService(CircuitUser):
         hash = hashlib.sha1(hash_string.encode('utf-8')).hexdigest()
         address = '{0}.onion'.format(hash)
         return address
+
+    # Divide website requested content into packet stream
+    def _get_site_packet_stream(self, hs_address, user_id, time):
+        packets = []
+        out_content = 'RP-data {0} {1}'.format(hs_address, user_id)
+        for i in range(int(math.ceil(self.size / PACKET_SIZE))-1):
+            packets.append(Packet(self.id, time, content=out_content))
+        out_content = 'RP-finish-data {0} {1}'.format(hs_address, user_id)
+        packets.append(Packet(self.id, time, content=out_content))
+        return packets
 
     # Function for initial setup of introduction point circuits
     def _setup_intro_points(self, intro_points):
@@ -47,11 +60,17 @@ class HiddenService(CircuitUser):
         self.hs_rp_circuits[(address, user_id)] = circuit
         rp.hs_circuits_map[(address, user_id)] = circuit
 
+    # Send website packets back through a circuit
+    def _send_website(self, hs_address, user_id, time):
+        packets = self._get_site_packet_stream(hs_address, user_id, time)
+        hs_rp_circuit = self.hs_rp_circuits[(hs_address, user_id)]
+        hs_rp_circuit.send_packets_to_startpoint(packets, self)
+
     def _process_packet(self, sender, packet, circuit=None):
         curtime = packet.creation_time + packet.lived
         in_content = packet.split(' ')
         packet_type = in_content[0]
-        if packet_type.startswith('RP-Establish'):
+        if packet_type.startswith('RP-establish'):
             rp_id = in_content[1]
             user_id = in_content[2]
             hs_address = in_content[3]
@@ -61,3 +80,8 @@ class HiddenService(CircuitUser):
             out_content = 'RP-HS {0} {1}'.format(hs_address, user_id)
             packet = Packet(self.id, curtime, content=out_content)
             self.hs_rp_circuits[(hs_address, user_id)].send_packets([packet], rp_relay)
+        elif packet_type.startswith('RP-GET'):
+            hs_address = in_content[1]
+            user_id = in_content[2]
+            self._send_website(user_id, hs_address)
+
