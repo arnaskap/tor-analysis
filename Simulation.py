004704d9d7c36a317c9caae8d5770624ff393f17
arnaskap <arnas.kapustinskas@gmail.com>
2018-02-27 10:36:49 +0000

    simulation setup implementation

--- Simulation.py
+++ Simulation.py
@@ -11,6 +11,9 @@ GUARD_RELAYS = 12
 MIDDLE_RELAYS = 24
 EXIT_RELAYS = 9
 
+TRACKED_GUARD_RELAYS = 2
+TRACKED_EXIT_RELAYS = 3
+
 CLEARNET_SITES = 20
 HIDDEN_SERVICES = 10
 
@@ -24,20 +27,47 @@ USERS_PER_RELAY = 10
 
 regions = ['Asia', 'Australia', 'Europe', 'North America', 'South America']
 
-
-def generate_relays(guard_num, middle_num, exit_num, delay):
-    guards, middles, exits = [], [], []
-    for i in range(guard_num + middle_num + exit_num):
-        if i < guard_num:
-            guards.append(Relay(i, 'guard', delay))
-        elif i < guard_num + middle_num:
-            middles.append(Relay(i, 'middle', delay))
-        else:
-            exits.append(Relay(i,'exit', delay))
-    return guards, middles, exits
-
 def get_region():
     return regions[random.randint(0, len(regions)-1)]
+
+def generate_relays(relay_bw, guard_num, middle_num, exit_num, tr_guards, tr_exits):
+    guards, middles, exits = [], [], []
+    tracked_guards, tracked_exits = [], []
+    all_relay_num = guard_num + middle_num + exit_num
+    guard_avg_bw = relay_bw * guard_num / all_relay_num
+    middle_avg_bw = relay_bw * middle_num / all_relay_num
+    exit_avg_bw = relay_bw * exit_num / all_relay_num
+    for i in range(guard_num):
+        id = 'g{0}'.format(str(i))
+        bw = abs(int(np.random.normal(guard_avg_bw, guard_avg_bw/10)))
+        region = get_region()
+        tracked = False
+        if tr_guards > 0:
+            tracked = True
+            tr_guards -= 1
+        relay = Relay(id, 'guard', bw, region, tracked=tracked)
+        guards.append(relay)
+        if tracked:
+            tracked_guards.append(relay)
+    for i in range(middle_num):
+        id = 'm{0}'.format(str(i))
+        bw = abs(int(np.random.normal(middle_avg_bw, middle_avg_bw/10)))
+        region = get_region()
+        relay = Relay(id, 'middle', bw, region)
+        middles.append(relay)
+    for i in range(exit_num):
+        id = 'e{0}'.format(str(i))
+        bw = abs(int(np.random.normal(exit_avg_bw, exit_avg_bw/10)))
+        region = get_region()
+        tracked = False
+        if tr_exits > 0:
+            tracked = True
+            tr_exits -= 1
+        relay = Relay(id, 'exit', bw, region, tracked=tracked)
+        exits.append(relay)
+        if tracked:
+            tracked_exits.append(relay)
+    return guards, middles, exits, tracked_guards, tracked_exits
 
 
 def generate_hidden_services(relays, amount):
@@ -52,57 +82,23 @@ def generate_hidden_services(relays, amount):
     return ips
 
 
-def generate_circuits(users_num, guards, middles, exits):
-    circuits = {}
-    for user in range(users_num):
-        circuit_guard = guards[random.randint(0, len(guards)-1)]
-        circuit_middle = middles[random.randint(0, len(middles)-1)]
-        circuit_exit = exits[random.randint(0, len(exits)-1)]
-        circuits[user] = (circuit_guard, circuit_middle, circuit_exit)
-    return circuits
-
-
-def generate_user_packets(circuit, user, avg_packets_sent, circuit_seconds):
-    total_packets = abs(int(np.random.normal(avg_packets_sent, 300.0)))
-    packet_times = np.random.uniform(low=0.0, high=circuit_seconds, size=(total_packets,))
-    for t in packet_times:
-        p1 = Packet(user, t)
-        p2 = circuit[0].add_packet(p1)
-        p3 = circuit[1].add_packet(p2)
-        circuit[2].add_packet(p3)
-
-
-def generate_relay_traffic(circuits, owned_guards, owned_exits, avg_packets_sent, circuit_seconds):
-    for u in circuits:
-        if circuits[u][0] not in owned_guards and circuits[u][2] not in owned_exits:
-            continue
-        generate_user_packets(circuits[u], u, avg_packets_sent, circuit_seconds)
-
-# def connec
-
-
-# lots of assumptions here
-def calculate_user_packets_per_circuit(bandwidth_ps, circuit_seconds, users):
-    # assumes all bandwidth contains user packets, none for establishing circuits
-    # div by 3 since same base packet is processed 3 times by different relays
-    packets_ps = bandwidth_ps // (PACKET_SIZE * 3)
-    packets_per_circut = packets_ps * circuit_seconds
-    avg_user_packets_per_circuit = packets_per_circut // users
-    return avg_user_packets_per_circuit
-
-
-def set_user_guards_owned(users, circuits, owned_guards):
-    for u in users:
-        circuits[u] = (owned_guards[random.randint(0, len(owned_guards)-1)], circuits[u][1], circuits[u][2])
-
-
-def set_user_exits_owned(users, circuits, owned_exits):
-    for u in users:
-        circuits[u] = (circuits[u][0], circuits[u][1], (owned_exits[random.randint(0, len(owned_exits) - 1)]))
-
-
 if __name__ == '__main__':
-    # guards, middles, exits = generate_relays(GUARD_RELAYS, MIDDLE_RELAYS, EXIT_RELAYS, UNIVERSAL_DELAY)
+    guard_num = GUARD_RELAYS
+    middle_num = MIDDLE_RELAYS
+    exit_num = EXIT_RELAYS
+    tracked_guards_num = TRACKED_GUARD_RELAYS
+    tracked_exits_num = TRACKED_EXIT_RELAYS
+
+    guards, middles, exits, tracked_guards, tracked_exits = generate_relays(guard_num,
+                                                                            middle_num,
+                                                                            exit_num,
+                                                                            tracked_guards_num,
+                                                                            tracked_exits_num)
+
+    generate_sites()
+    generate_hidden_services()
+    generate_users()
+
     time = 0
     exit = Relay('e1', 'exit', 200000, 'North America', tracked=True)
     guard = Relay('g1', 'guard', 100000, 'Asia', tracked=True)
--- Users.py
+++ Users.py
@@ -5,16 +5,49 @@
 # 2 - visits hidden services as often as clearnet sites, high probability to revisit same sites
 # 3 - visits hidden services as often as clearnet sites, picks visited websites randomly
 
+import random
+
+
 class User:
 
-    def __init__(self, client, type):
+    def __init__(self, client, type, sites, hidden_services):
         self.client = client
         self.type = type
+        self.sites = sites
+        self.hidden_services = hidden_services
+        self.visited_sites = []
+        self.visited_hidden_services = []
+
+    def _visit_site_by_prob(self, pr_hs, pr_new):
+        visit_hs_chance = random.randint(1, pr_hs)
+        visit_same_chance = random.randint(1, pr_new)
+        if visit_hs_chance < pr_hs:
+            if visit_same_chance < pr_new and self.visited_sites:
+                which_site = random.randint(0, len(self.visited_sites) - 1)
+                site_to_visit = self.visited_sites[which_site]
+                self.client.visit_clearnet_site(site_to_visit)
+            else:
+                which_site = random.randint(0, len(self.sites) - 1)
+                site_to_visit = self.sites[which_site]
+                self.client.visit_clearnet_site(site_to_visit)
+                if site_to_visit not in self.visited_sites:
+                    self.visited_sites.append(site_to_visit)
+        else:
+            if visit_same_chance < pr_new and self.visited_hidden_services:
+                which_hs = random.randint(0, len(self.visited_hidden_services) - 1)
+                hs_to_visit = self.visited_sites[which_hs]
+                self.client.visit_clearnet_site(hs_to_visit)
+            else:
+                which_hs = random.randint(0, len(self.hidden_services) - 1)
+                hs_to_visit = self.sites[which_hs]
+                self.client.visit_clearnet_site(hs_to_visit)
+                if hs_to_visit not in self.visited_hidden_services:
+                    self.visited_hidden_services.append(hs_to_visit)
 
     def visit_next(self):
         if self.type == 1:
-            pass
+           self._visit_site_by_prob(5, 10)
         elif self.type == 2:
-            pass
+            self._visit_site_by_prob(2, 10)
         elif self.type == 3:
-            pass
+            self._visit_site_by_prob(2, 2)
